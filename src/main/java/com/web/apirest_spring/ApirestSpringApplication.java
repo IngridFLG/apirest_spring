855e0d59a214681c2559760e8b1b3325e24baa12
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-06-14 15:15:10 -0500

    primer commit

---
+++ src/main/java/com/web/apirest_spring/ApirestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.web.apirest_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApirestSpringApplication.class, args);
+    }
+
+}
